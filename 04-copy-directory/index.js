f97cf33aeaad30b2878e2bc604571d9ba9053a4b
codestudent24 <belgorod2407@yandex.ru>
2023-05-07 21:29:52 +1000

    feat: complette tasks 1-4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,16 +6,25 @@ console.log('start')
 const sourcePath = path.resolve(__dirname, 'files');
 const destPath = path.resolve(__dirname, 'files-copy')
 
+fs.mkdir(destPath, {recursive: true}, err => {
+    if (err) throw err;
+})
+
 function copyDir(pathFrom, pathTo) {
     fs.readdir(pathFrom, function (err, files) {
         for (let i = 0; i < files.length; i++) {
             const filePath = path.join(pathFrom, files[i]);
             const newPathTo = path.join(pathTo, files[i]);
-            console.log('from ' + filePath + ' to ' + newPathTo)
+            //console.log('from ' + filePath + ' to ' + newPathTo)
             fs.stat(filePath, (error, stats) => {
                 if (stats.isFile()) {
-                    fs.copyFile(filePath, newPathTo)
+                    fs.copyFile(filePath, newPathTo, (err) => {
+                        if (err) throw err;
+                    })
                 } else {
+                    fs.mkdir(newPathTo, {recursive: true}, err => {
+                        if (err) throw err;
+                    })
                     copyDir(filePath, newPathTo);
                 }
             })
@@ -25,3 +34,13 @@ function copyDir(pathFrom, pathTo) {
 }
 
 copyDir(sourcePath, destPath)
+
+/*fs.mkdir(destPath, {recursive: true}, err => {
+    if (err) throw err;
+})
+console.log(path.join(sourcePath, 'test-css.css'))
+console.log(path.join(destPath, 'test-css.css'))
+fs.copyFile(path.join(sourcePath, 'test-css.css'),
+    path.join(destPath, 'test-css.css'), (err) => {
+        if (err) throw err;
+    })*/
